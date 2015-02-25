006642298033aa7619f6d8392f26ffaefbfb5ef0
kaidez <kai.gittens@gmail.com>
2015-02-24 21:32:52 -0500

    comment out enque of twentythirteen stylsheet...for now

--- themes/parlor/functions.php
+++ themes/parlor/functions.php
@@ -1,9 +1,9 @@
 <?php
   // Import the Twenty Fifteen stylesheet to the parlor theme
 	// This will (probably) be deleted from the final build
-  add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-	function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-	}
+  // add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+	// function theme_enqueue_styles() {
+  //   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	// }
 
 ?>
