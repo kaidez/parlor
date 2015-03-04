7aa1ef1494408f1c7cd949428e13de226e92154a
kaidez <kai.gittens@gmail.com>
2015-03-04 04:58:28 -0500

    update facebook link copy, put it in a <span>

--- themes/parlor/header.php
+++ themes/parlor/header.php
@@ -25,7 +25,7 @@
     <div class="contact-information-inner">
       <span class="contact-information-element phone-number">t: 973.746.1182</span>
 			<span class="contact-information-element address">223 glenridge ave, montclair, nj 07042</span>
-			<a href="https://www.facebook.com/pages/Parlor-Hair-Studio/153741038024144" class="facebook-link">Parlor On Facebook</a>
+			<a href="https://www.facebook.com/pages/Parlor-Hair-Studio/153741038024144" class="facebook-link"><span class="facebook-link-text">Visit Parlor On Facebook</span></a>
 			 <span class="contact-information-element hours">hours: tues 10-7 / wed. 10-8 / thurs &amp; fri 11-8 / sat 10-5</span>
 		</div>
 	</div> <!-- contact-information -->
