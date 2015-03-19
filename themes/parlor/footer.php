2b79c8f4e12034651142e3005fb62da411ec9ef8
kaidez <kai.gittens@gmail.com>
2015-03-19 17:50:16 -0400

    add link to the sitemap

--- themes/parlor/footer.php
+++ themes/parlor/footer.php
@@ -14,7 +14,7 @@
 <footer id="colophon" class="site-footer" role="contentinfo">
 	<div class="site-footer-inner">
 	  <span class="copyright">copyright 2006-15 Parlor Hair Studio</span>
-		<span class="sitemap-link">sitemap</span>
+		<span class="sitemap-link"><a href="/wp/sitemap/">sitemap</a></span></span>
 	</div>
 </footer><!-- #colophon -->
 </div><!-- #page -->
