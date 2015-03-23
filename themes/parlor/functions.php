9b8670f1fc9fa0d25ca313ac3085c327505b7d74
kaidez <kai.gittens@gmail.com>
2015-03-23 06:39:58 -0400

    remove commented-out code from parlot functions.php

--- themes/parlor/functions.php
+++ themes/parlor/functions.php
@@ -1,11 +1,4 @@
 <?php
-  // Import the Twenty Thirteen stylesheet to the parlor theme
-	// This will (probably) be deleted from the final build
-
-  // add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-	// function theme_enqueue_styles() {
-  //   wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-	// }
   
   // Remove extra <p> tags and (I think) <br /> tags from page content
   remove_filter('the_content', 'wpautop');
