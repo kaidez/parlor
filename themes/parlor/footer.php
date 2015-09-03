3b11645ce17049c11a44afdc48d7de51b13b1ddb
Kai Gittens <kai.gittens@gmail.com>
2015-09-02 22:26:39 -0400

    add instagram link to the footer

--- themes/parlor/footer.php
+++ themes/parlor/footer.php
@@ -14,6 +14,7 @@
 <footer id="colophon" class="site-footer" role="contentinfo" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
 	<div class="site-footer-inner">
 	  <span class="copyright">copyright 2006-15 Parlor Hair Studio</span>
+    <span class="instagram"><a href="https://instagram.com/parlorhairstudio/">follow us on instagram</a></span>
 		<span class="sitemap-link"><a href="/sitemap/">sitemap</a></span></span>
 	</div>
 </footer><!-- #colophon -->
